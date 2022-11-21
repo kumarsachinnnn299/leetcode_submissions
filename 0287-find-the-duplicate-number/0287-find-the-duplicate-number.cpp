7be9bdf9dd61d31e61abee637323741df2773f65
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2022-11-22 00:21:32 +0530

    Update 0287-find-the-duplicate-number.cpp

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,4 +1,4 @@
-// https://www.youtube.com/watch?v=32Ll35mhWg0&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2
+// https://leetcode.com/problems/find-the-duplicate-number/discuss/73045/Simple-C%2B%2B-code-with-O(1)-space-and-O(nlogn)-time-complexity
 
 class Solution {
 public:
