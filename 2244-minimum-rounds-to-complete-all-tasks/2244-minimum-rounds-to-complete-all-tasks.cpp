1341000f4edfe17eddb50637e270233aed20942d
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-01-11 02:10:52 +0530

    Time: 239 ms (89.86%), Space: 104.7 MB (17.66%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // Easy question
+    // https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/discuss/2995189/Easiest-C%2B%2B-solution-with-detailed-explanation
     int minimumRounds(vector<int>& tasks) {
         map<int,int>mp;
         
@@ -8,16 +11,7 @@ public:
             mp[i]++;
         }
         int ans=0;
-        cout<<"60: "<<mp[60]<<endl;
-        cout<<"61: "<<mp[61]<<endl;
-        cout<<"62: "<<mp[62]<<endl;
-        cout<<"63: "<<mp[63]<<endl;
-        cout<<"64: "<<mp[64]<<endl;
-        cout<<"66: "<<mp[65]<<endl;
-        cout<<"66: "<<mp[66]<<endl;
-        cout<<"67: "<<mp[67]<<endl;
-        cout<<"68: "<<mp[68]<<endl;
-         cout<<"69: "<<mp[69]<<endl;
+        
         for(auto i:mp)
         {   int temp=i.second;
            if(temp==1)return -1;
