b4c91d977f3c3639b3a5d98ec20ae8e6dcace8cf
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-01-11 11:34:10 +0530

    Time: 190 ms (85.57%), Space: 60.5 MB (64.77%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,3 +1,6 @@
+
+// https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/discuss/623679/C%2B%2B-Short-and-simple-DFS
+
 class Solution {
 public:
     vector<vector<int>> adjList;
@@ -8,17 +11,16 @@ public:
 	    if(i!=prev)
 	    {
 	        temp=dfs(hasApple,i,d+1,node);
-	        if(temp)			//If child has apples it'll return a non zero result which is the distance traveled upto that node.
+	        if(temp)			
 		    result+=temp-d;
 	    }
-        return result||hasApple[node]?result+d:0;  //If nothing is added to result and current node doesnt have apple return 0 else return distances of children + current distance from root.
-        
+        return result||hasApple[node]?result+d:0;  
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) 
     {
         adjList.resize(n);
         for(vector<int> &e:edges)
             adjList[e[0]].push_back(e[1]),adjList[e[1]].push_back(e[0]);
-        return dfs(hasApple,0,0,-1)*2;     //Result is doubled the distance travelled as per our observation.
+        return dfs(hasApple,0,0,-1)*2;     
     }
 };
