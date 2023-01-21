08e69ac16546e1a0648047b007407343dba07be6
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-01-21 13:07:37 +0530

    Added solution - LeetHub

---
+++ Minimum X (xor) A - GFG/minimum-x-xor-a.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minVal(int a, int b) {
+        
+           int setbitsA=__builtin_popcount(a);
+        int setbitsB=__builtin_popcount(b);
+        if(setbitsA==setbitsB)return a;
+        while(setbitsA>setbitsB)
+        {
+            a&=(a-1);
+            setbitsA--;
+        }
+        while(setbitsB>setbitsA)
+        {
+            a=a|(a+1);
+            setbitsB--;
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        Solution ob;
+        cout << ob.minVal(a, b);
+
+        cout << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
