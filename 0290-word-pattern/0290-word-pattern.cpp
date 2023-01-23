44e8b64ad4f7e513b348fee3cfe851c6ff8e0cdf
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-01-24 01:54:41 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (25.31%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        
+        vector<string>words;
+        string temp="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' ')
+            {
+                words.push_back(temp);
+                temp="";
+            }
+            else{
+                temp+=s[i];
+            }
+        }
+        words.push_back(temp);
+        map<char,string>mp;
+        if(pattern.size()!=words.size())return false;
+        set<string>st;
+        for(int i=0;i<pattern.size();i++)
+        {
+            if(mp.find(pattern[i])!=mp.end())
+            {
+                if(mp[pattern[i]]!=words[i])return false;
+            }
+            else{
+               if(st.count(words[i])>0)return false;
+                mp[pattern[i]]=words[i];
+                st.insert(words[i]);
+            }
+        }
+        return true;
+        
+    }
+};
