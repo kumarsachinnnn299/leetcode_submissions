b0d55b733e37149637037faf15bf70a43d4c82be
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-01 01:40:04 +0530

    Time: 40 ms (38.07%), Space: 41.2 MB (51.26%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -14,27 +14,29 @@
 
 class Solution {
 public:
-    // Serialize subtrees and check for duplicates using a post-order traversal
-    string serializeSubtrees(TreeNode* node, unordered_map<string, int>& subtrees, vector<TreeNode*>& duplicates) {
-        if (!node) return "#"; // Null nodes are represented by '#'
-        
-        string left = serializeSubtrees(node->left, subtrees, duplicates);
-        string right = serializeSubtrees(node->right, subtrees, duplicates);
-        
-        string s = left + "," + right + "," + to_string(node->val); // Serialize the current subtree
-        
-        if (subtrees[s] == 1) duplicates.push_back(node); // If a duplicate subtree is found, add to the vector
-        
-        subtrees[s]++;
+    vector<TreeNode*>ans;
+        unordered_map<string,int>mp;
+
+    
+    string helper(TreeNode* root)
+    {   
+       if(root==NULL)return "#";
+        string s=to_string(root->val)+" "+helper(root->left)+"rn"+helper(root->right)+"ln";
+        if(mp[s]==1&&s!="#"){
+            ans.push_back(root);
+        cout<<s<<endl;}
+        mp[s]++;
         return s;
-    }
- vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        unordered_map<string, int> subtrees; // Store serialized subtree and its frequency
-        vector<TreeNode*> duplicates; // Store duplicate subtrees
         
-        serializeSubtrees(root, subtrees, duplicates); // Traverse the tree and serialize subtrees
-        
-        return duplicates;
     }
     
+    
+    
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        
+        helper(root);
+      
+        return ans;
+        
+    }
 };
