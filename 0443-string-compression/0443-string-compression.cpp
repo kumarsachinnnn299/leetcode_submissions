8bcd3d45c89fd880504e15cddeb29d79824fcdd2
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-03 03:06:02 +0530

    Time: 3 ms (92.73%), Space: 9 MB (82.85%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int compress(vector<char>& arr) {
+     string ans="";
+        ans+=arr[0];
+        int c=1;
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]==arr[i-1])
+            {
+                c++;
+            }
+            else{
+                if(c>1)
+               { ans+=to_string(c);
+                c=1;
+               }
+                ans+=arr[i];
+            }
+        }
+        if(c>1)ans+=to_string(c);
+        for(int i=0;i<ans.size();i++)arr[i]=ans[i];
+        return ans.size();
+        
+    }
+};
