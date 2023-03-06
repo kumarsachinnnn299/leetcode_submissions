7c67181d0b518251f3a547eabe771d96fd994ab4
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-07 02:25:10 +0530

    Time: 3 ms (60.45%), Space: 7.6 MB (24.53%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head->next==NULL)return head;
+        if(left==right)return head;
+        ListNode* leftend=NULL,*rightend=NULL,*temp=head,*st,*end,*prev=NULL,*nxt;
+        int c=1;
+        while(c!=left&&temp!=NULL)
+        {
+            leftend=temp;
+            temp=temp->next;
+            c++;
+        }
+        st=temp;
+        rightend=temp->next;
+         while(c!=right&&temp!=NULL)
+        {
+           
+            temp=temp->next;
+             rightend=temp->next;
+            c++;
+        }
+        end=temp;
+        cout<<st->val;
+        
+        while(st!=rightend)
+        {
+         nxt=st->next;
+        st->next=prev;
+            prev=st;
+            st=nxt;
+        }
+        if(leftend!=NULL)
+        {
+            leftend->next=prev;
+       //      while(leftend->next!=NULL)
+       // {
+       //     leftend=leftend->next;
+       // }
+       //  if(rightend!=NULL)leftend->next=rightend;
+        }
+        temp=head;
+        if(rightend!=NULL)
+        {while(temp->next!=NULL)temp=temp->next;
+        temp->next=rightend;}
+       
+        
+        if(leftend==NULL)return prev;
+        return head;
+        
+            
+        
+    }
+};
