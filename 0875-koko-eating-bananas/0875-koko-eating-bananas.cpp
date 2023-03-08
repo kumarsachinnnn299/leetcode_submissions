42b2625908d5b8e1065f9e37bd3eb7a166ed42cf
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-08 18:43:07 +0530

    Time: 53 ms (55.53%), Space: 19 MB (11.65%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    bool isvalid(int gspeed,vector<int>&arr,int h)
+    {
+        int time=0;
+        for(auto i:arr)
+        {
+            time+=((i/gspeed)+(i%gspeed!=0));
+            if(time>h)return false;
+        }
+        return true;
+    }
+    
+    int minEatingSpeed(vector<int>& arr, int h) {
+        sort(arr.begin(),arr.end());
+        int s=1,e=*max_element(arr.begin(),arr.end());
+        int ans=INT_MAX;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(isvalid(mid,arr,h))
+            {   ans=mid;
+                e=mid-1;
+            }
+            else s=mid+1;
+        }
+        return s;
+    }
+};
