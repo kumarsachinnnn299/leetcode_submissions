d546cc33e29f07e9d9c82ce070763c1e6cde9164
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-10 17:04:42 +0530

    Time: 3 ms, Memory: 9.1 MB - LeetHub

---
+++ third-maximum-number/third-maximum-number.cpp
@@ -0,0 +1,33 @@
+//https://www.youtube.com/watch?v=NYzAm7nhpdk
+//Thoda isse resemble kiya
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        if(n==2)return max(nums[0],nums[1]);
+        long long max1=LONG_MIN,max2=LONG_MIN,max3=LONG_MIN;
+        for(auto i:nums)
+        {
+            if(i>max1)
+            {
+                max3=max2;
+                max2=max1;
+                max1=i;
+            }
+            else if(i>max2&i!=max1)
+            {
+                max3=max2;
+                max2=i;
+            }
+            else if(i>max3&&i!=max2&&i!=max1)
+            {
+                max3=i;
+            }
+        }
+        
+        if(max2==LONG_MIN||max3==LONG_MIN)return max1;
+        return max3;
+    }
+};
