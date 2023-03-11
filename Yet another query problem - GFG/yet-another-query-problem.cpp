87bf179624b60147bd72b72f82adb2d31b4a6593
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-11 17:20:59 +0530

    Added solution - LeetHub

--- Yet another query problem - GFG/yet-another-query-problem.cpp
+++ Yet another query problem - GFG/yet-another-query-problem.cpp
@@ -8,6 +8,9 @@ class Solution {
   public:
     vector<int> solveQueries(int N, int num, vector<int> &A, vector<vector<int>> &Q) {
         // code here
+        
+        //Khud kiya
+        
         vector<int>ans;
         map<int,int>mp2;
         vector<int>frequency(N);
@@ -16,25 +19,12 @@ class Solution {
             mp2[A[i]]++;
             frequency[i]=mp2[A[i]];
         }
-        
-        // for(auto i:frequency)cout<<i<<' ';
-        // cout<<endl;
-        
         for(auto i:Q)
         {
-            // map<int,int>mp;
-            // mp=mp2;
-            
+         
             int c=0;
             
-        // if(i[0]>0)
-        // {
-        //     int run=i[0];
-        //     for(int j=0;j<run;j++)
-        //     {
-        //         mp[A[j]]--;
-        //     }
-        // }
+ 
         
         for(int j=i[0];j<=i[1];j++){
             // if(mp[A[j]]==i[2])c++;
