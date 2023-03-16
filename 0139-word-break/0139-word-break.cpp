6db899b85b1b5f116fda5e6ff7150d050ad9d679
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-16 22:32:57 +0530

    Time: 14 ms (55.40%), Space: 9.3 MB (71.71%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[301];
+    
+    int helper(string s,int start,set<string>&st)
+    {   if(start==s.size())return 1;
+        if(dp[start]!=-1)return dp[start];
+        string temp="";
+        for(int i=start;i<s.size();i++)
+        {
+            temp+=s[i];
+            if(st.find(temp)!=st.end()&&helper(s,i+1,st))return 1;
+        }
+        return dp[start]=0;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string>st;
+        memset(dp,-1,sizeof dp);
+        for(auto i:wordDict)st.insert(i);
+        return helper(s,0,st);
+    }
+};
