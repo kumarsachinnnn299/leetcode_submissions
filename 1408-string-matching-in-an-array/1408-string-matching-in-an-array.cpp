ad1e1e269e5fa039a41d1d7fbca068d399fd1675
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-17 02:12:38 +0530

    Time: 4 ms (78.63%), Space: 8.3 MB (72.85%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    static bool comp(const string &s1,const string &s2){
-        return s1.size() < s2.size();
-    }
+    
+    // bool issubstring(string small,string large)
+    // {
+    //     if(large.find(small)!=-1)return true;
+    //     return false;
+    // }
+    
     vector<string> stringMatching(vector<string>& words) {
-        sort(words.begin(),words.end(),comp);
         vector<string>ans;
-        for(int i=0;i<words.size();i++){
-            for(int j=i+1;j<words.size();j++){
-                if(words[j].find(words[i]) != -1){
-                    ans.push_back(words[i]);
-                    break;
-                }
+        sort(words.begin(),words.end(),[](const string  &s1,const string & s2){
+            return s1.size()<s2.size();
+        });
+      
+        for(int i=0;i<words.size();i++)
+        {
+            for(int j=i+1;j<words.size();j++)
+            {
+                
+                    if(words[j].find(words[i])!=-1){
+                        ans.push_back(words[i]);
+                    break;}
+                
+                
             }
         }
         return ans;
