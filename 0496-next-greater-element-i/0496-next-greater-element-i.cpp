1a2ef0000ec08f5941c0b5c38ccf051359d316f0
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-21 20:55:42 +0530

    Time: 9 ms (48.70%), Space: 9.1 MB (10.29%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,30 +1,26 @@
- //Self-> Adarsh video concept
-
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        map<int,int>mp;
         stack<int>st;
-        unordered_map<int,int>mp;
         for(int i=nums2.size()-1;i>=0;i--)
         {
-            if(st.empty())mp[nums2[i]]=-1;
-            else if(st.top()>nums2[i])mp[nums2[i]]=st.top();
-            else{
-                while(!st.empty()&&st.top()<=nums2[i])st.pop();
-                if(st.empty())mp[nums2[i]]=-1;
-                else mp[nums2[i]]=st.top();
+            if(st.empty())
+            {
+                mp[nums2[i]]=-1;
             }
+            
+            while(!st.empty()&&st.top()<=nums2[i])st.pop();
+            if(!st.empty())mp[nums2[i]]=st.top();
+            else mp[nums2[i]]=-1;
             
             st.push(nums2[i]);
         }
- 
-        
-         vector<int>ans;
-        for(int i=0;i<nums1.size();i++)
+        for(auto i:nums1)
         {
-            ans.push_back(mp[nums1[i]]);
+            ans.push_back(mp[i]);
         }
         return ans;
-        
     }
 };
