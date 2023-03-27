89b9b1b7970fdd22f0d27b2b2e20926aaa576e08
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-03-27 10:06:40 +0530

    Time: 673 ms (23.92%), Space: 174.4 MB (13.29%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        map<long long,vector<long long>>mp;
+        vector<bool>vis(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]].push_back(i);
+        }
+        long long ans=0;
+        for(auto i:mp)
+        {
+            for(auto j:i.second)
+            {
+                if(vis[j]==false)
+                {
+                    ans+=nums[j];
+                    vis[j]=true;
+                    if(j<nums.size()-1)vis[j+1]=true;
+                    if(j>0)vis[j-1]=true;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
