6a10b5f276298d8c010c07552539ba66efefb809
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-07 23:49:46 +0530

    Time: 132 ms (8.83%), Space: 70 MB (5.01%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,55 @@
+//similar to https://leetcode.com/problems/number-of-closed-islands/
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+         int dir_x[4] = {1, -1, 0, 0};
+        int dir_y[4] = {0, 0, 1, -1};
+
+        int ans = 0;  
+
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<bool>> vis(m, vector(n, false));
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                if(vis[i][j] || grid[i][j] == 0) 
+                    continue;
+
+                queue<pair<int, int>> q({{i, j}});
+
+                bool is_closed = true;
+                int cells=0;
+                vis[i][j]=true;
+
+                while(!q.empty()){
+
+                    auto front = q.front();
+                    q.pop();
+                    cells++;
+
+                    for(int k = 0; k < 4; k++){
+
+                        int dy = front.first + dir_y[k];
+                        int dx = front.second + dir_x[k];
+                        
+                        if(dy < 0 || dy >= m || dx < 0 || dx >= n) { 
+                            is_closed = false;
+                            continue;
+                        }
+                        if(grid[dy][dx] == 0) continue;
+                        if(vis[dy][dx]) continue;
+                        vis[dy][dx] = true;
+                        q.push({dy, dx});
+                    }
+                }
+                if(is_closed) 
+                    ans+=cells;
+            }
+        }
+
+        return ans;
+    }
+};
