5945eb86fe28e23cbca5d92d8470ae3ba2c5f94b
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-11 23:18:53 +0530

    Added solution - LeetHub

--- Find first set bit - GFG/find-first-set-bit.cpp
+++ Find first set bit - GFG/find-first-set-bit.cpp
@@ -18,7 +18,7 @@ class Solution
         // Your code here
         n&=(-1*n);
         int ans=1;
-        while(n)
+        for(int i=0;i<32;i++)
         {
             if(n&1)return ans;
             ans++;
