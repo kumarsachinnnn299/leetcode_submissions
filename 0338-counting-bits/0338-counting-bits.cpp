a49a984e98168d90b0226909623a1ee207b396c6
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-11 23:34:31 +0530

    Time: 8 ms (43.70%), Space: 8.5 MB (36.66%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,27 @@
+//By kernighams algo
+//https://www.youtube.com/watch?v=I475waWiTK4&list=PL-Jc9J83PIiFJRioti3ZV7QabwoJK6eKe&index=3
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int>ans;
+        ans.push_back(0);
+        for(int i=1;i<=n;i++)
+        {
+            int c=0;
+            int x=i;
+            int mask=x&(-1*x);
+            while(true)
+            {   if(mask==0)break;
+                c++;
+                x-=mask;
+                mask=(x&(-1*x));
+            }
+            ans.push_back(c);
+        }
+        return ans;
+        
+        
+    }
+};
