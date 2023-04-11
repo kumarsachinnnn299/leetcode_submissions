d290f07cf60b48e2f6afbf96945f72c46795c089
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-11 11:10:34 +0530

    Time: 95 ms (44.42%), Space: 25 MB (91.01%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,26 +1,9 @@
+
+
 class Solution {
 public:
     string removeStars(string s) {
-//         vector<int>arr;
-//         int st=0,e=s.size()-1;
-//         for(int i=0;i<s.size();i++)
-//         {
-            
-//         }
-//         stack<char>st;
-//         for(auto i:s)
-//         {
-//             if(i!='*')st.push(i);
-//             else st.pop();
-//         }
-        
-//         string ans="";
-//         while(!st.empty())
-//         {
-//             ans=st.top()+ans;
-//             st.pop();
-//         }
-//         return ans;
+
         int c=0;
         bool flag=false;
         
