c819c09de24cc9a29891395b7ffaff4e06cb7cca
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-13 00:34:54 +0530

    Time: 31 ms (48.80%), Space: 22.3 MB (62.87%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,27 @@
+// https://leetcode.com/problems/single-element-in-a-sorted-array/discuss/3212178/Day-52-oror-Binary-Search-oror-Easiest-Beginner-Friendly-Sol
+
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int s=0,e=nums.size()-1;
         if(nums.size()==1)return nums[0];
         while(s<=e)
-        {
-        if(nums[s]!=nums[s+1])return nums[s];
-            if(nums[e]!=nums[e-1])return nums[e];
-            s+=2;
-            e-=2;
+        {   
+            int mid=s+(e-s)/2;
+            if(mid%2==0)
+            {
+                if(nums[mid+1]!=nums[mid])e=mid-1;
+                else s=mid+1;
+            }
+            else{
+                if(nums[mid-1]!=nums[mid])e=mid-1;
+                else s=mid+1;
+            }
         }
-        return 0;
+        
+       // if(s==e)return nums[s];
+        cout<<s<<' '<<e<<endl;
+        
+        return nums[e+1];
     }
 };
