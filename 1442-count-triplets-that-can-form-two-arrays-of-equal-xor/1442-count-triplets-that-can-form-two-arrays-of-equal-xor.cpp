68a56d798ca8cba61a4a35d5723a7d8f3cefb93d
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-14 00:55:17 +0530

    Time: 3 ms (82.63%), Space: 7.3 MB (97.52%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,3 +1,7 @@
+// https://www.youtube.com/watch?v=sdJBPdoD3OY&list=PL-Jc9J83PIiFJRioti3ZV7QabwoJK6eKe&index=13
+
+//Sumit bits playlist
+
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
