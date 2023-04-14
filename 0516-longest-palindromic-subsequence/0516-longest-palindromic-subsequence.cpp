586eb03953ee856e92b13b0d20c381c48f3563f6
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-14 12:54:56 +0530

    Time: 256 ms (17.36%), Space: 72.9 MB (63.49%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -9,21 +9,19 @@ public:
     int longestPalindromeSubseq(string text1) {
         string text2=text1;
         reverse(text2.begin(),text2.end());
-        vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1));
-        int ans=INT_MAX;
+        vector<vector<int>>dp(text1.size()+1,vector<int>(text1.size()+1));
+        
         for(int i=1;i<=text1.size();i++)
         {
-            for(int j=1;j<=text2.size();j++)
+            for(int j=1;j<=text1.size();j++)
             {
-                if(text1[i-1]==text2[j-1])
-                {
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }
+                if(text1[i-1]==text2[j-1])dp[i][j]=dp[i-1][j-1]+1;
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
-        return dp[text1.size()][text2.size()];
+        return dp[text1.size()][text1.size()];
     }
 };
