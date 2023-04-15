3708ddf99b32a1d7c0d706703cad0d82b9dd5c76
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-15 23:47:56 +0530

    Time: 7 ms (39.32%), Space: 12.1 MB (41.76%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,32 @@
+// https://www.youtube.com/watch?v=-h1FHkDxpKs&list=PLjeQ9Mb66hM3hgJfIvc32sTRQOpZ2SXxC&index=5
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+       int n=nums.size()-2;
+        int i;
+        for( i=n;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])break;
+            
+        }
+        
+        if(i<0)
+            {
+                sort(nums.begin(),nums.end());
+                return;
+            }
+        
+        for(int j=nums.size()-1;j>i;j--)
+        {
+            if(nums[j]>nums[i])
+            {
+                swap(nums[i],nums[j]);
+                sort(nums.begin()+i+1,nums.end());
+                break;
+            }
+        }
+        
+        
     }
 };
