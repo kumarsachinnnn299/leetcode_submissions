64ac1e0dbbb3cd48a1fe1a22ffe14df0b9557db0
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-20 22:27:57 +0530

    Added solution - LeetHub

--- Bheem Wants Ladoos - GFG/bheem-wants-ladoos.cpp
+++ Bheem Wants Ladoos - GFG/bheem-wants-ladoos.cpp
@@ -106,48 +106,59 @@ class Solution{
     {
         // Your code goes here
         int ans=0;
-        queue<Node*> q;
+        unordered_map<Node*,Node*>parent;
+        queue<Node*>q;
         q.push(root);
-        Node* need;
-        unordered_map<Node*,Node*> m;
-        while(q.size()){
-            int s = q.size();
-            for(int i=0;i<s;i++){
-                Node* temp = q.front();
-                q.pop();
-                if(temp->data==home)need=temp;
-                if(temp->left){
-                    q.push(temp->left);
-                    m[temp->left]=temp;
-                }
-                if(temp->right){
-                    q.push(temp->right);
-                    m[temp->right]=temp;
-                }
+        Node*target;
+        unordered_map<Node*,bool>vis;
+        // parent[root]=NULL;
+        while(!q.empty())
+        {   int s=q.size();
+            for(int i=0;i<s;i++)
+            {
+                 Node*temp=q.front();
+            q.pop();
+            if(temp->data==home)target=temp;
+            if(temp->left!=NULL)
+            {
+                q.push(temp->left);
+                parent[temp->left]=temp;
             }
+             if(temp->right!=NULL)
+            {
+                q.push(temp->right);
+                parent[temp->right]=temp;
+            }
+            }
+           
         }
-        unordered_map<Node*,bool> mm;
-        q.push(need);
-        int dis=0;
-        while(q.size()){
-            int s = q.size();
-            for(int i=0;i<s;i++){
-                Node* temp = q.front();
+        
+        q.push(target);
+        
+        int d=0;
+        while(!q.empty())
+        {   int s=q.size();
+            for(int i=0;i<s;i++)
+            {
+                Node*temp=q.front();
                 q.pop();
-                mm[temp]=true;
-                ans+=temp->data;
-                if(temp->left && mm[temp->left]==false){
-                    q.push(temp->left);
+                ans+=(temp->data);
+                vis[temp]=true;
+                if(temp->left!=NULL&&vis[temp->left]==false)
+                {
+                q.push(temp->left);
+               
                 }
-                if(temp->right && mm[temp->right]==false){
-                    q.push(temp->right);
+                if(temp->right!=NULL&&vis[temp->right]==false)
+                {
+                q.push(temp->right);
+               
                 }
-                if(m[temp] && mm[m[temp]]==false){
-                    q.push(m[temp]);
-                }
+            
+                if(parent[temp]!=NULL&&vis[parent[temp]]==false)q.push(parent[temp]);
             }
-            dis++;
-            if(dis>k)break;
+            d++;
+            if(d>k)break;
         }
         return ans;
         
