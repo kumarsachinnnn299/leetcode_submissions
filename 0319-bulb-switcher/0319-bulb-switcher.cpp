278389d9a36fd665135255ca33bb457d12805997
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-27 15:31:59 +0530

    Time: 3 ms (45.34%), Space: 5.9 MB (63.99%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,9 +1,11 @@
+//Just see pattern for 1 to 10 simply get it
+
 class Solution {
 public:
     int bulbSwitch(int n) {
         
         
-        int sqr=sqrt(n);
-        return sqr;
+        return sqrt(n);
+        
     }
 };
