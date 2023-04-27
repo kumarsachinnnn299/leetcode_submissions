e7e827bdc66314b740b11c712085048229e2aa8d
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-04-28 00:45:02 +0530

    Time: 17 ms (64.52%), Space: 9.1 MB (15.27%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -7,9 +7,11 @@ public:
         int ans=0;
         int hcf=1;
         for(int i=0;i<nums.size();i++)
-        {   hcf=nums[i];
+        {   
+            
+            hcf=nums[i];
             for(int j=i;j<nums.size();j++)
-            {
+            {   if(nums[i]<k)break;
                 hcf=__gcd(hcf,nums[j]);
                 if(hcf<k)break;
                 if(hcf==k)ans++;
