3ac70a42f5203c60f1c9fe6f816613caa8b57cf9
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-05 01:26:41 +0530

    Time: 6 ms (82.22%), Space: 8.1 MB (12.22%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -3,56 +3,32 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
-        
-    int cr=0,cd=0;
-        for(int  i=0;i<senate.size();i++)
+    
+        queue<int>rad,dire;
+        int n=senate.size();
+        for(int i=0;i<n;i++)
         {
-            if(senate[i]=='R')cr++;
-            else cd++;
+            if(senate[i]=='R')rad.push(i);
+            else dire.push(i);
             
         }
-        int i=0,n=senate.size();
-        while(cr&&cd)
+        while(!rad.empty()&&!dire.empty())
         {
-            if(senate[i]!='@')
+            if(rad.front()<dire.front())
             {
-                if(senate[i]=='R')
-                {
-                    
-                    int temp=(i+1)%n;
-                    while(true)
-                    {
-                        if(senate[temp]=='D'){
-                            senate[temp]='@';
-                            cd--;
-                            break;
-                        }
-                        temp++;
-                        temp%=n;
-                    }
-                }
-                else{
-                    int temp=(i+1)%n;
-                    while(true)
-                    {
-                        if(senate[temp]=='R'){
-                            senate[temp]='@';
-                            cr--;
-                            break;
-                        }
-                        temp++;
-                        temp%=n;
-                    }
-                }
+                dire.pop();
+                rad.push(rad.front()+n);
+                rad.pop();
+                
             }
-            
-                i++;
-                i%=n;
-            
-          
+            else{
+                rad.pop();
+                dire.push(dire.front()+n);
+                dire.pop();
+            }
         }
-        if(cr)return "Radiant";
-        return "Dire";
+        if(dire.size())return "Dire";
+        return "Radiant";
     }
     
 };
