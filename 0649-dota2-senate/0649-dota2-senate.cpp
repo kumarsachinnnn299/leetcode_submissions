dfcf46c37475aae62a2ac79b4b2214aebd62345a
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-05 01:15:16 +0530

    Time: 467 ms (5.18%), Space: 6.8 MB (97.04%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        
+    int cr=0,cd=0;
+        for(int  i=0;i<senate.size();i++)
+        {
+            if(senate[i]=='R')cr++;
+            else cd++;
+            
+        }
+        int i=0,n=senate.size();
+        while(cr&&cd)
+        {
+            if(senate[i]!='@')
+            {
+                if(senate[i]=='R')
+                {
+                    
+                    int temp=(i+1)%n;
+                    while(true)
+                    {
+                        if(senate[temp]=='D'){
+                            senate[temp]='@';
+                            cd--;
+                            break;
+                        }
+                        temp++;
+                        temp%=n;
+                    }
+                }
+                else{
+                    int temp=(i+1)%n;
+                    while(true)
+                    {
+                        if(senate[temp]=='R'){
+                            senate[temp]='@';
+                            cr--;
+                            break;
+                        }
+                        temp++;
+                        temp%=n;
+                    }
+                }
+            }
+            
+                i++;
+                i%=n;
+            
+          
+        }
+        if(cr)return "Radiant";
+        return "Dire";
+    }
+    
+};
