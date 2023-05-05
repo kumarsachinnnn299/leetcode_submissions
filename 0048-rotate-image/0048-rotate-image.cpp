07df4170ea8473bfa95e961f6d5a7d6b38054de1
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-05 15:55:26 +0530

    Time: 6 ms (12.02%), Space: 6.9 MB (93.77%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,19 @@
+//transpose and reverse each row
+//half triangle hi traverse kre either upper or lower
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+     for(int i=0;i<matrix.size();i++)
+     {
+         for(int j=0;j<i;j++)//upper trianlge
+         {
+             swap(matrix[i][j],matrix[j][i]);
+         }
+     }
+        for(int i=0;i<matrix.size();i++)
+        {
+            
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
