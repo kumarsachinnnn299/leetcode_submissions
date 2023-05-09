1077e79bff89ac8d29506841134766788139148c
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-10 01:06:49 +0530

    Added solution - LeetHub

---
+++ Total Cuts - GFG/total-cuts.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int totalCuts(int N,int K,vector<int> &A){
+        // Code here
+        if(N==1)return 0;
+        vector<int>maxl,minr(N-1);
+        maxl.push_back(A[0]);
+        minr[minr.size()-1]=A[N-1];
+        for(int i=2;i<N;i++ )
+        {
+            if(A[i-1]>maxl[maxl.size()-1])maxl.push_back(A[i-1]);
+            else maxl.push_back(maxl[maxl.size()-1]);
+        }
+        
+        for(int i=N-3;i>=0;i--)
+        {
+            if(A[i+1]<minr[i+1])
+            {
+                minr[i]=A[i+1];
+            }
+            else minr[i]=minr[i+1];
+        }
+        
+        // for(auto i:maxl)cout<<i<<' ';
+        // cout<<endl;
+        //  for(auto i:minr)cout<<i<<' ';
+        // cout<<endl;
+        
+        int ans=0;
+        for(int i=0;i<N-1;i++)
+        {
+            if(maxl[i]+minr[i]>=K)ans++;
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N,K;
+        cin>>N>>K;
+        vector<int> A(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        Solution ob;
+        cout<<ob.totalCuts(N,K,A)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
