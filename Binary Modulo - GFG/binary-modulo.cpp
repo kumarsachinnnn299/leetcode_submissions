6a697981fae410a670dd345700c143eeee464a5e
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-09 21:54:24 +0530

    Added solution - LeetHub

--- Binary Modulo - GFG/binary-modulo.cpp
+++ Binary Modulo - GFG/binary-modulo.cpp
@@ -7,6 +7,7 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+// https://www.youtube.com/watch?v=twFvNBbgypM
 class Solution{
     public:
         int modulo(string s,int m)
