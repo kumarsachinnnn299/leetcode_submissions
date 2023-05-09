d427261c2b3a244d1e966cf71d75b1f3dc2b5e0b
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-09 11:05:57 +0530

    Time: 4 ms (33.67%), Space: 6.9 MB (30.23%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,39 +1,39 @@
+// https://www.youtube.com/watch?v=CywIodsJI7w&list=PLjeQ9Mb66hM2xgXW01bdC8luDS8iyZSMF&index=10
+
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& arr) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int>ans;
-        int c1=0,c2=arr[0].size(),r1=0,r2=arr.size();
-        while(c1<c2&&r1<r2)
+        int r1=0,r2=matrix.size(),c1=0,c2=matrix[0].size();
+        while(r1<r2&&c1<c2)
         {
-            for(int i =c1;i<c2;i++)
+            for(int i=c1;i<c2;i++)
             {
-                ans.push_back(arr[r1][i]);
+                ans.push_back(matrix[r1][i]);
             }
             r1++;
             for(int i=r1;i<r2;i++)
             {
-                ans.push_back(arr[i][c2-1]);
+                ans.push_back(matrix[i][c2-1]);
             }
             c2--;
-            
             if(r1<r2)
             {
                 for(int i=c2-1;i>=c1;i--)
                 {
-                    ans.push_back(arr[r2-1][i]);
+                    ans.push_back(matrix[r2-1][i]);
                 }
-                 r2--;
+                r2--;
+               
             }
-           
-            if(c1<c2)
-            {
-                for(int i=r2-1;i>=r1;i--)
+             if(c2>c1)
                 {
-                    ans.push_back(arr[i][c1]);
+                    for(int i=r2-1;i>=r1;i--)
+                    {
+                        ans.push_back(matrix[i][c1]);
+                    }
+                 c1++;
                 }
-                c1++;
-            }
-            
         }
         return ans;
     }
