d85d322bce8913825aa24eccffee2029ad84c96a
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-11 16:59:25 +0530

    Time: 20 ms (13.24%), Space: 12.4 MB (88.28%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,25 +1,49 @@
-//Khud kiya
+//Khud kiya map use krke
+
+//Priority que approach: https://www.youtube.com/watch?v=dT-XiXKYbDI
 
 class Solution {
 public:
+    class cmp{
+        public:
+         bool operator()(pair<int,string>&p1, pair<int,string>&p2)
+        {
+            if(p1.first!=p2.first)return p1.first>p2.first;
+                else return p1.second<p2.second;
+        }
+    };
+   
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map<string,int>mp;
-        map<int,set<string>,greater<int>>mp2;
+        unordered_map<string,int>mp;
         for(auto i:words)mp[i]++;
+        priority_queue<pair<int,string>,vector<pair<int,string>>,cmp>pq;
+        
         for(auto i:mp)
         {
-            mp2[i.second].insert(i.first);
-        }
-        vector<string>ans;
-        for(auto i:mp2)
-        {
-            for(auto j:i.second)
-            {
-                ans.push_back(j);
-                if(ans.size()==k)return ans;
+            if(pq.size()<k)pq.push({i.second,i.first});
+            else{
+                if(i.second>pq.top().first)
+                {
+                    pq.pop();
+                    pq.push({i.second,i.first});
+                }
+                else if(i.second==pq.top().first&&i.first<pq.top().second)
+                {
+                    pq.pop();
+                    pq.push({i.second,i.first});
+                }
             }
         }
+        vector<string>ans;
+
+        while(!pq.empty())
+        {   string s=pq.top().second;
+            ans.push_back(s);
+            
+            pq.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
-        
+       
     }
 };
