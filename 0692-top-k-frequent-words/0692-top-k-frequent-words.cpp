2635f32a4e62d9a6b6601ebcdedf0082303d9100
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-11 16:07:07 +0530

    Time: 12 ms (71.74%), Space: 13.3 MB (6.07%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string,int>mp;
+        map<int,set<string>,greater<int>>mp2;
+        for(auto i:words)mp[i]++;
+        for(auto i:mp)
+        {
+            mp2[i.second].insert(i.first);
+        }
+        vector<string>ans;
+        for(auto i:mp2)
+        {
+            for(auto j:i.second)
+            {
+                ans.push_back(j);
+                if(ans.size()==k)return ans;
+            }
+        }
+        return ans;
+        
+    }
+};
