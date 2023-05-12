8c6296636102b8cfb81bb65d30e55e0c9e88bdc3
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-05-12 14:55:50 +0530

    Time: 391 ms (35.51%), Space: 123 MB (44.72%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    
+public:
+    
+    long long helper(vector<long long>&dp,vector<vector<int>>&arr,int cidx)
+    {
+        if(cidx>=arr.size())return 0;
+        if(dp[cidx]!=-1)return dp[cidx];
+        long long taken=(long long)(arr[cidx][0])+helper(dp,arr,cidx+arr[cidx][1]+1);
+        long long  nottaken=helper(dp,arr,cidx+1);
+        return dp[cidx]=max(taken,nottaken);
+    }
+    
+    long long mostPoints(vector<vector<int>>& questions) {
+       vector<long long>dp(questions.size(),-1);
+        long long ans=0;
+        ans=helper(dp,questions,0);
+        return ans;
+    }
+};
