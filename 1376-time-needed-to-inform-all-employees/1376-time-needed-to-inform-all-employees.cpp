c86f9b31eebdef566198a8690a1e593c2d9d55af
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-06-03 22:38:34 +0530

    Time: 348 ms (40.25%), Space: 124.4 MB (39.81%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,33 @@
+//Simple bfs question
+
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        int ans=informTime[headID];
+        queue<int>q;
+        vector<vector<int>>g(n);
+        for(int i=0;i<manager.size();i++)
+        {
+            if(manager[i]!=-1)
+            {
+                g[manager[i]].push_back(i);
+            }
+        }
+        q.push(headID);
+        while(!q.empty())
+        {
+            int curr=q.front();
+            q.pop();
+            for(int i=0;i<g[curr].size();i++)
+            {
+                int next=g[curr][i];
+                informTime[next]+=informTime[curr];
+                ans=max(ans,informTime[next]);
+                q.push(next);
+            }
+        }
+        return ans;
+            
+        
+    }
+};
