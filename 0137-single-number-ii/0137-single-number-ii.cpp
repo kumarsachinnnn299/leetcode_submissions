75451731aabe38aa00f738551463ece241a3e65e
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-07-04 11:37:33 +0530

    Time: 3 ms (98.52%), Space: 9.4 MB (72.62%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    if(nums.size()==1)return nums[0];
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i<nums.size()-2&&nums[i]==nums[i+1]&&nums[i]==nums[i+2])
+            {
+                i+=2;
+            }
+            
+            else{
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
