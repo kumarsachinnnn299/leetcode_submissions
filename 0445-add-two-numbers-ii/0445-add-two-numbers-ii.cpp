80eda39dd30edd7458eb29552d822bca062d76b4
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-07-17 13:14:03 +0530

    Time: 51 ms (18.64%), Space: 73.6 MB (22.79%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int prev=0;
+        stack<int>st;
+        ListNode* r1=l1,*r2=l2;
+      vector<int>n1,n2,n3;
+        while(r1)
+        {   n1.push_back(r1->val);
+            r1=r1->next;
+        }
+        while(r2)
+        {   n2.push_back(r2->val);
+             r2=r2->next;
+        }
+        
+        // for(auto i:n1)cout<<i<<' ';
+        // cout<<endl;
+        // for(auto i:n2)cout<<i<<' ';
+        // cout<<endl;
+        
+       int c1=n1.size()-1,c2=n2.size()-1;
+        while(c1>=0&&c2>=0)
+        {
+            int sum=n1[c1]+n2[c2]+prev;
+            n3.push_back(sum%10);
+            prev=sum/10;
+            c1--;
+            c2--;
+        }
+        
+        while(c1>=0)
+        {
+            int sum=n1[c1]+prev;
+            n3.push_back(sum%10);
+            prev=sum/10;
+            c1--;
+        }
+        
+          while(c2>=0)
+        {
+            int sum=n2[c2]+prev;
+            n3.push_back(sum%10);
+            prev=sum/10;
+            c2--;
+        }
+        
+        if(prev)n3.push_back(prev);
+        ListNode*ans,*ptr;
+        ans=new ListNode(n3[n3.size()-1]);
+        ptr=ans;
+        
+            for(int i=n3.size()-2;i>=0;i--){
+                ptr->next=new ListNode(n3[i]);
+                ptr=ptr->next;
+            }
+                
+                return ans;
+    }
+};
