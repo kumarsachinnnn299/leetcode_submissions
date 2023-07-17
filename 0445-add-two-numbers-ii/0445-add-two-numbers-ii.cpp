243652e1f1ddcf2adc39a72d1c10e0385e9e95e1
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-07-17 13:30:42 +0530

    Time: 36 ms (73.80%), Space: 71.2 MB (47.34%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -12,22 +12,21 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* r1, ListNode* r2) {
         int prev=0;
-        // ListNode* r1=l1,*r2=l2;
-      vector<int>n1,n2,n3;
+      string n1,n2,n3;
         while(r1)
-        {   n1.push_back(r1->val);
+        {   n1+=to_string(r1->val);
             r1=r1->next;
         }
+        
         while(r2)
-        {   n2.push_back(r2->val);
+        {   n2+=to_string(r2->val);
              r2=r2->next;
-        }
-           
+        }       
        int c1=n1.size()-1,c2=n2.size()-1;
         while(c1>=0&&c2>=0)
         {
-            int sum=n1[c1]+n2[c2]+prev;
-            n3.push_back(sum%10);
+            int sum=(n1[c1]-'0')+(n2[c2]-'0')+prev;
+            n3=to_string(sum%10)+n3;
             prev=sum/10;
             c1--;
             c2--;
@@ -35,30 +34,31 @@ public:
         
         while(c1>=0)
         {
-            int sum=n1[c1]+prev;
-            n3.push_back(sum%10);
+            int sum=(n1[c1]-'0')+prev;
+            n3=to_string(sum%10)+n3;
             prev=sum/10;
             c1--;
         }
         
           while(c2>=0)
         {
-            int sum=n2[c2]+prev;
-            n3.push_back(sum%10);
+            int sum=(n2[c2]-'0')+prev;
+            n3=to_string(sum%10)+n3;
             prev=sum/10;
             c2--;
         }
         
-        if(prev)n3.push_back(prev);
+        if(prev)n3=to_string(prev)+n3;
         ListNode*ans,*ptr;
-        ans=new ListNode(n3[n3.size()-1]);
+        ans=new ListNode(n3[0]-'0');
         ptr=ans;
         
-            for(int i=n3.size()-2;i>=0;i--){
-                ptr->next=new ListNode(n3[i]);
+            for(int i=1;i<n3.size();i++){
+                ptr->next=new ListNode(n3[i]-'0');
                 ptr=ptr->next;
             }
                 
+      
                 return ans;
     }
 };
