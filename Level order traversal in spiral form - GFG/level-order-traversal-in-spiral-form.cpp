a2eca37afd0eebb3a90434aa8f434d347a5a8b9c
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-07-25 09:51:49 +0530

    Added solution - LeetHub

--- Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.cpp
+++ Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.cpp
@@ -125,32 +125,29 @@ struct Node
     }
 }; */
 
-// https://www.youtube.com/watch?v=mUGFMHBvA50
-//Done using stack
 
-// https://www.youtube.com/watch?v=mUGFMHBvA50
 //Function to return a list containing the level order traversal in spiral form.
 vector<int> findSpiral(Node *root)
 {
     //Your code here
-    stack<Node*>st1,st2;//st1-> right to left, st2->left to right
-    st1.push(root);
     vector<int>ans;
+    stack<Node*>st1,st2;
+    st1.push(root);
     while(!st1.empty()||!st2.empty())
     {
         while(!st1.empty())
         {
             auto temp=st1.top();
-            ans.push_back(temp->data);
             st1.pop();
+            ans.push_back(temp->data);
             if(temp->right)st2.push(temp->right);
             if(temp->left)st2.push(temp->left);
         }
         while(!st2.empty())
         {
             auto temp=st2.top();
-            ans.push_back(temp->data);
             st2.pop();
+            ans.push_back(temp->data);
             if(temp->left)st1.push(temp->left);
             if(temp->right)st1.push(temp->right);
         }
