c9fad6a9602fd72304b2b1b1c68effa5540b4181
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-07-26 10:17:39 +0530

    Time: 622 ms (5.03%), Space: 101.4 MB (67.19%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -3,23 +3,7 @@
 class Solution {
 public:
     
-    bool isvalid(int mid,double hour,vector<int>&dist)
-    {   double temp=0;
-        for(int i=0;i<dist.size();i++)
-        {
-            if(i==dist.size()-1)
-            {temp+=((double)(dist[i])/(double)(mid));
-            }
-            else {
-                temp+=(dist[i]/mid)+(dist[i]%mid!=0);
-                
-            }
-            
-            
-        }
-     return (temp<=hour);
-     
-    }
+
     
     int minSpeedOnTime(vector<int>& dist, double hour) {
         int s=1,e=1e8;
@@ -27,14 +11,24 @@ public:
         while(s<=e)
         {
             int mid=s+(e-s)/2;
-            if(isvalid(mid,hour,dist)){
+            double temp=0;
+             for(int i=0;i<dist.size();i++)
+            {
+                if(i==dist.size()-1)
+                {temp+=((double)(dist[i])/(double)(mid));
+                }
+                else {
+                    temp+=(dist[i]/mid)+(dist[i]%mid!=0);
+
+                }
+
+            }
+            if(temp<=hour)
+            {
                 ans=mid;
                 e=mid-1;
             }
-            else 
-            {
-                s=mid+1;
-            }
+            else s=mid+1;
         }
         return ans;
     }
