73934d9708cb3da8576148926de59b02d4962878
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-03 18:18:22 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (82.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,70 +1,95 @@
 class Solution
 {
 public:
-	vector<string> letterCombinations(string digits)
-	{
-		vector<string> ans;
-		if (digits == "")
-			return ans;
-		map<char, string> mp;
-		mp['2'] = "abc";
-		mp['3'] = "def";
-		mp['4'] = "ghi";
-		mp['5'] = "jkl";
-		mp['6'] = "mno";
-		mp['7'] = "pqrs";
-		mp['8'] = "tuv";
-		mp['9'] = "wxyz";
-		for (char i : mp[digits[0]])
-		{   
-			if (digits.size() > 1)
-			{
-				for (char  j : mp[digits[1]])
-				{   
-					if (digits.size() > 2)
-					{
-						for (char  k : mp[digits[2]])
-						{   cout<<k<<endl;
-							if (digits.size() > 3)
-							{
-								for (char  l : mp[digits[3]])
-								{
-									string temp = "";
-									temp += i ;
-                                    temp += j ;
-                                temp += k;
-                                temp += l;
-									ans.push_back(temp);
-								}
-							}
-							else
-							{
-								string temp = "";
-								temp += i ;
-                                temp += j;
-                                temp += k;
-								ans.push_back(temp);
-							}
-						}
-					}
-					else
-					{
-						string temp = "";
-						temp += i;
-                        temp += j;
-                        cout<<temp<<endl;
-						ans.push_back(temp);
-					}
-				}
-			}
+  vector<string> letterCombinations(string s)
+  {
+    if (s.size() == 0)
+      return {};
+    vector<string> ans;
+    unordered_map<char, string> mp;
+    mp['2'] = "abc";
+    mp['3'] = "def";
+    mp['4'] = "ghi";
+    mp['5'] = "jkl";
+    mp['6'] = "mno";
+    mp['7'] = "pqrs";
+    mp['8'] = "tuv";
+    mp['9'] = "wxyz";
 
-			else
-			{
-				string temp = "";
-				temp = i;
-				ans.push_back(temp);
-			}
-		}
-		return ans;
-	}
+   
+      string temp = mp[s[0]];
+      if (s.size() > 1)
+      {
+        string temp2 = mp[s[1]];
+
+        if (s.size() > 2)
+        {
+          string temp3 = mp[s[2]];
+
+          if (s.size() > 3)
+          {
+            string temp4 = mp[s[3]];
+            for(auto m:temp)
+           { for (auto j : temp2)
+            {
+              for (auto k : temp3)
+              {
+                for (auto l : temp4)
+                {
+                  string f = "";
+                  f += m;
+                  f+=j;
+                  f+=k;
+                  f+=l;
+                  ans.push_back(f);
+                }
+              }
+            }}
+          }
+
+          else
+          {
+            for(auto l:temp)
+            {for (auto j : temp2)
+            {
+              for (auto k : temp3)
+              {
+               string t="";
+               t+=l;
+               t+=j;
+               t+=k;
+                ans.push_back(t);
+              }
+            }}
+          }
+        }
+
+        else
+        {
+
+          for (auto j : temp)
+          {
+            for(auto k:temp2)
+            { string f="";
+              f+=j;
+              f+=k;
+              ans.push_back(f);
+            }
+          }
+        }
+      }
+
+      else
+      {
+
+        for (auto j : temp)
+        {
+          string t = "";
+          t += j;
+          ans.push_back(t);
+        }
+      }
+    
+    return ans;
+  }
 };
