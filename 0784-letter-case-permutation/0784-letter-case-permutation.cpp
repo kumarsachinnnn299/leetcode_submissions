3a3907efb9fb483266baa060342120163a4a8be7
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-06 17:55:55 +0530

    Time: 33 ms (6.11%), Space: 32.4 MB (5.39%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    void helper(int idx,unordered_set<string>&ans,vector<string>arr,string s)
+    {
+        if(idx==arr[0].size())
+        {
+            ans.insert(s);
+            return;
+        }
+        
+        string s2=s;
+        s2+=arr[0][idx];
+        s+=arr[1][idx];
+        helper(idx+1,ans,arr,s);
+         helper(idx+1,ans,arr,s2);
+        
+    }
+    
+    vector<string> letterCasePermutation(string s) {
+        vector<string>ans,arr;
+        arr.push_back(s);
+        unordered_set<string>st;
+        string s2="";
+        for(auto i:s){
+            if(i-'a'>=0)s2+=toupper(i);
+            else s2+=tolower(i);
+            
+        }
+        arr.push_back(s2);
+        s2="";
+        helper(0,st,arr,s2);
+        for(auto i:st)ans.push_back(i);
+        return ans;
+    }
+};
