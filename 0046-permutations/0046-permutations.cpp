ad5b9c6679aa03662e7c33a9005c693bc7ba76e7
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-08 01:18:13 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (59.44%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,24 @@
-// https://www.youtube.com/watch?v=R3Sm9V2OSCo
-
-
 class Solution {
 public:
     
-    void backtrack(vector<vector<int>>&ans, vector<int>&arr, vector<int>nums, vector<bool>vis)
+    void helper(int idx,vector<int>&nums,vector<vector<int>>&ans)
     {
-        if(arr.size()==nums.size()){
-            ans.push_back(arr);
-        return;
-            }
-        
-        for(int i=0;i<nums.size();i++)
+        if(idx==nums.size())
         {
-            if(vis[i]==false){
-                vis[i]=true;
-                arr.push_back(nums[i]);
-                backtrack(ans,arr,nums,vis);
-                arr.pop_back();
-                vis[i]=false;
-            }
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=idx;i<nums.size();i++)
+        {
+            swap(nums[idx],nums[i]);
+            helper(idx+1,nums,ans);
+            swap(nums[idx],nums[i]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<bool>vis(nums.size());
-        vector<int>arr;
-        backtrack(ans,arr,nums,vis);
+        helper(0,nums,ans);
         return ans;
     }
 };
