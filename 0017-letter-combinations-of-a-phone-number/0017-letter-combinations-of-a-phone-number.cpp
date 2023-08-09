80e39427f8fb5a5053b5a5f521b95a30bbd8ba39
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-09 16:10:56 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (42.10%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,95 +1,42 @@
-class Solution
-{
+//Recusrsion by Fraz
+
+class Solution {
 public:
-  vector<string> letterCombinations(string s)
-  {
-    if (s.size() == 0)
-      return {};
-    vector<string> ans;
-    unordered_map<char, string> mp;
-    mp['2'] = "abc";
-    mp['3'] = "def";
-    mp['4'] = "ghi";
-    mp['5'] = "jkl";
-    mp['6'] = "mno";
-    mp['7'] = "pqrs";
-    mp['8'] = "tuv";
-    mp['9'] = "wxyz";
-
-   
-      string temp = mp[s[0]];
-      if (s.size() > 1)
-      {
-        string temp2 = mp[s[1]];
-
-        if (s.size() > 2)
-        {
-          string temp3 = mp[s[2]];
-
-          if (s.size() > 3)
-          {
-            string temp4 = mp[s[3]];
-            for(auto m:temp)
-           { for (auto j : temp2)
+    void helper(int idx,string& dig,unordered_map<char,string>&mp,vector<string>&ans,string &temp)
+    {
+     if(idx==dig.size())
+     {
+         ans.push_back(temp);
+         return;
+     }
+        string s=mp[dig[idx]];
+            for(int i=0;i<s.size();i++)
             {
-              for (auto k : temp3)
-              {
-                for (auto l : temp4)
-                {
-                  string f = "";
-                  f += m;
-                  f+=j;
-                  f+=k;
-                  f+=l;
-                  ans.push_back(f);
-                }
-              }
-            }}
-          }
-
-          else
-          {
-            for(auto l:temp)
-            {for (auto j : temp2)
-            {
-              for (auto k : temp3)
-              {
-               string t="";
-               t+=l;
-               t+=j;
-               t+=k;
-                ans.push_back(t);
-              }
-            }}
-          }
-        }
-
-        else
-        {
-
-          for (auto j : temp)
-          {
-            for(auto k:temp2)
-            { string f="";
-              f+=j;
-              f+=k;
-              ans.push_back(f);
+                temp.push_back(s[i]);
+                helper(idx+1,dig,mp,ans,temp);
+                temp.pop_back();
             }
-          }
-        }
-      }
-
-      else
-      {
-
-        for (auto j : temp)
-        {
-          string t = "";
-          t += j;
-          ans.push_back(t);
-        }
-      }
     
-    return ans;
-  }
+    
+        
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0)return {};
+        unordered_map<char,string>letter;
+            letter['2']="abc";
+            letter['3']="def";
+            letter['4']="ghi";
+            letter['5']="jkl";
+            letter['6']="mno";
+            letter['7']="pqrs";
+            letter['8']="tuv";
+            letter['9']="wxyz";
+      vector<string>ans;
+        string temp="";
+         helper(0,digits,letter,ans,temp);
+        return ans;
+     
+          
+    }
 };
