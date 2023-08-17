b69567aa0fd6013039ccacb7f965a8b812c22630
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-17 22:41:48 +0530

    Added solution - LeetHub

---
+++ Print N-bit binary numbers having more 1s than 0s - GFG/print-nbit-binary-numbers-having-more-1s-than-0s.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+    
+    void helper(int idx,string &temp,vector<string>&ans,int N,int zc,int oc)
+    {
+        if(idx>N||oc<zc||oc>N||zc>N)return;
+        if(zc<=oc&&temp.size()==N)ans.push_back(temp);
+        temp+='1';
+        oc++;
+        helper(idx+1,temp,ans,N,zc,oc);
+        temp.pop_back();
+        oc--;
+        temp+='0';
+        zc++;
+         helper(idx+1,temp,ans,N,zc,oc);
+        temp.pop_back();
+        zc--;
+         
+        
+        
+        
+    }
+
+	vector<string> NBitBinary(int N)
+	{
+	    // Your code goes here
+	    vector<string>ans;
+	    string temp="1";
+	    int z=0,o=1;
+	    helper(1,temp,ans,N,z,o);
+	    return ans;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		int n;
+   		cin >> n;
+        Solution ob;
+   		vector<string> ans = ob.NBitBinary(n);
+
+   		for(auto i:ans)
+   			cout << i << " ";
+
+   		cout << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
