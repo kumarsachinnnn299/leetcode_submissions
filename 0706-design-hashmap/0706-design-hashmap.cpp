f37441a59204cb50fa2f4c4a0461abbe2b40e409
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-17 22:24:29 +0530

    Time: 621 ms (5.02%), Space: 331.3 MB (5.22%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,59 @@
+//Fraz playlist
+//https://www.youtube.com/watch?v=xVEGczCJCHs&list=PLjkkQ3iH4jy-QRceXhVwYjJ7ToNsBs0Il&index=10
+
+class MyHashMap {
+public:
+    vector<list<pair<int,int>>>arr;
+    int n;
+    MyHashMap() {
+        int n=1e6+1;//kuch bhi le skte h
+        arr.resize(n);
+    }
+    
+    int hash(int key)
+    {
+        return key%n;
+    }
+    
+    
+    list<pair<int,int>>::iterator search(int key)
+    {   
+        int i=hash(key);
+        list<pair<int,int>>::iterator itr;
+        itr=arr[i].begin();
+        while(itr!=arr[i].end())
+        {
+            if(itr->first==key)return itr;
+            itr++;
+        }
+        return itr;
+    }
+    
+    void put(int key, int value) {
+        int i=hash(key);
+        remove(key);
+        arr[i].push_back({key,value});
+    }
+    
+    int get(int key) {
+        int i=hash(key);
+         list<pair<int,int>>::iterator itr=search(key);
+        if(itr==arr[i].end())return -1;
+        return itr->second;
+    }
+    
+    void remove(int key) {
+        int i=hash(key);
+         list<pair<int,int>>::iterator itr=search(key);
+        if(itr==arr[i].end())return;
+        arr[i].erase(itr);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
