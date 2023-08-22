f36cc336806a5132d7c370cbfa8200aa69611c15
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-22 22:42:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (33.99%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        long long  ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            ans=((ans*26)+s[i]-'A'+1);
+        }
+        return ans;
+    }
+};
