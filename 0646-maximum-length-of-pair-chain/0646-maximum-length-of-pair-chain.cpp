8b0a57044fe8c3145a56a9c2204f0b9c275b0d85
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-08-26 17:07:42 +0530

    Time: 198 ms (59.02%), Space: 22.8 MB (55.03%) - LeetHub

---
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+     int helper(int idx,vector<vector<int>>&nums,vector<int>&dp)
+    {   
+        if(dp[idx]!=-1)return dp[idx];
+        int ans=1;
+        for(int i=0;i<idx;i++)
+        {   if(nums[idx][0]>nums[i][1])
+            ans=max(ans,helper(i,nums,dp)+1);
+        }
+        return dp[idx]=ans;
+    }
+    
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end());
+        vector<int>dp(pairs.size(),-1);
+        
+        int ans=1;
+        
+        for(int i=0;i<pairs.size();i++)
+        {
+            ans=max(ans,helper(i,pairs,dp));
+        }
+        return ans;
+        
+    }
+};
