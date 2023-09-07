7865f248e420f6fbbb580c0e81a72a0ce2bf615d
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-09-07 19:11:38 +0530

    Time: 4 ms (38.20%), Space: 7.5 MB (12.22%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//Khud kiya
+
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
