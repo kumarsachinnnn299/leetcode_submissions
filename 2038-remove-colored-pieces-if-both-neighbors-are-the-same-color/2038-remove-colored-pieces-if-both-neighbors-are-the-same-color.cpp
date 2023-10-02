fe45ee220b2c53b03fe4caee2ee20749256b2bbd
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-10-02 13:36:05 +0530

    Time: 38 ms (24.97%), Space: 13.7 MB (17.42%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool winnerOfGame(string s) {
+        
+        int ca=0,cb=0;
+         int sA=0,sB=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='A')
+            {   if(sB>2)cb+=(sB-2);
+                sB=0;
+                sA++;
+            }
+            else{
+                if(sA>2)ca+=(sA-2);
+                sA=0;
+                sB++;
+            }
+        }
+        
+        if(sA&&sA>2)ca+=(sA-2);
+        if(sB&&sB>2)cb+=(sB-2);
+        
+        // cout<<ca<<' '<<cb;
+        // if(ca==0)return false;
+        if(ca>cb)return true;
+            return false;
+        
+    }
+};
