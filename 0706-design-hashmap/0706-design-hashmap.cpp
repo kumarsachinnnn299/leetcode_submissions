9c8f6204d37c81b2e3c6c563530490c464c63964
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-10-04 20:38:06 +0530

    Time: 99 ms (86.19%), Space: 208.5 MB (11.05%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,52 +1,23 @@
-//Fraz playlist
-//https://www.youtube.com/watch?v=xVEGczCJCHs&list=PLjkkQ3iH4jy-QRceXhVwYjJ7ToNsBs0Il&index=10
-
 class MyHashMap {
 public:
-    vector<list<pair<int,int>>>arr;
-    int n;
+    
+    int arr[1000001];
+    
     MyHashMap() {
-        int n=1000001;//kuch bhi le skte h
-        arr.resize(n);
-    }
-    
-    int hash(int key)
-    {
-        return key%n;
-    }
-    
-    
-    list<pair<int,int>>::iterator search(int key)
-    {   
-        int i=hash(key);
-        list<pair<int,int>>::iterator itr;
-        itr=arr[i].begin();
-        while(itr!=arr[i].end())
-        {
-            if(itr->first==key)return itr;
-            itr++;
-        }
-        return itr;
+        memset(arr,-1,sizeof(arr));
     }
     
     void put(int key, int value) {
-        int i=hash(key);
-        remove(key);
-        arr[i].push_back({key,value});
+        arr[key]=value;
     }
     
     int get(int key) {
-        int i=hash(key);
-         list<pair<int,int>>::iterator itr=search(key);
-        if(itr==arr[i].end())return -1;
-        return itr->second;
+        if(arr[key]!=-1)return arr[key];
+        return -1;
     }
     
     void remove(int key) {
-        int i=hash(key);
-         list<pair<int,int>>::iterator itr=search(key);
-        if(itr==arr[i].end())return;
-        arr[i].erase(itr);
+        arr[key]=-1;
     }
 };
 
