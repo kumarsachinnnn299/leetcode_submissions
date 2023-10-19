f7599fbb6255f830e13ae2dee485858a14a8ee16
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-10-19 21:38:02 +0530

    Time: 3 ms (47.11%), Space: 6.7 MB (29.19%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string s1="",t1="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='#'&&s1.size())s1.pop_back();
+            else if(s[i]=='#')continue;
+            else s1+=s[i];
+            
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=='#'&&t1.size())t1.pop_back();
+            else if(t[i]=='#')continue;
+            else t1+=t[i];
+        }
+        
+        cout<<s1<<' '<<t1;
+        
+        return s1==t1;
+    }
+};
