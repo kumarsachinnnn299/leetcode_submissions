ccfe49c0953093a6311fc93a77de8453e2656ad4
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-10-27 18:51:34 +0530

    Time: 128 ms (46.55%), Space: 8.6 MB (64.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,30 +1,35 @@
+//Done
+//Already submitted
+
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.length();
-        bool dp[n][n];
+        int n=s.size();
+        string s2=s;
         int st,e;
+        bool dp[n][n];
         for(int gap=0;gap<n;gap++)
         {
             for(int i=0,j=gap;j<n;j++,i++)
             {
                 if(gap==0)dp[i][j]=true;
-                else if(gap==1)
-                {
-                    dp[i][j]=(s[i]==s[j]);
-                }
-                else{
-                    dp[i][j]=((s[i]==s[j])&&dp[i+1][j-1]);
-                }
+                else if(gap==1)dp[i][j]= (s[i]==s[j]);
+                else dp[i][j]=((s[i]==s[j])&&(dp[i+1][j-1]));
                 if(dp[i][j])
                 {
                     st=i;
                     e=j;
                 }
             }
+            
         }
-        string ans="";
-        for(int i=st;i<=e;i++)ans+=s[i];
+        string ans;
+        for(int i=st;i<=e;i++)
+        {
+            ans+=s[i];
+        }
         return ans;
+            
+        
     }
 };
