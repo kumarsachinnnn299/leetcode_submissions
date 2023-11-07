55dc7fd14aa10ec1d1326a2e4ff6484c461ecbba
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-11-07 15:00:03 +0530

    Time: 109 ms (63.20%), Space: 85.5 MB (40.00%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
@@ -8,27 +8,20 @@ public:
             arr.push_back(temp);
         }
         sort(arr.begin(),arr.end());
-        for(auto i:arr)cout<<i<<' ';
-        cout<<endl;
+      
         bool charged=true;
         int ans=1;
         int time=1;
         int idx=1;
         while(idx<arr.size())
         {
-            if(charged)
-            {
+            
                 if(arr[idx]<=time)return ans;
                 time++;
                 ans++;
-                charged=false;
+                
                 idx++;
-            }
-            else{
-                charged=true;
-                // time++;
-               
-            }
+            
         }
         return ans;
     }
