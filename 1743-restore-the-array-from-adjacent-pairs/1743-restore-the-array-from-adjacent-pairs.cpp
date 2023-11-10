8a8e99ff23cb77b5f4c6cd4c58d5d2e8e6c041b2
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-11-11 00:36:49 +0530

    Time: 717 ms (9.40%), Space: 173.4 MB (6.67%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,34 @@
+// https://www.youtube.com/watch?v=emF5eAYR3Nk
+
+class Solution {
+public:
+    
+    void dfs(map<int,vector<int>>&mp,set<int>&vis,int a,vector<int>&ans)
+    {
+        if(vis.find(a)!=vis.end())return;
+        vis.insert(a);
+        ans.push_back(a);
+        for(auto i:mp[a])
+        {
+            dfs(mp,vis,i,ans);
+        }
+    }
+    
+    vector<int> restoreArray(vector<vector<int>>& arr) {
+        map<int,vector<int>>mp;
+        for(auto i:arr)
+        {
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+        }
+        int head;
+        for(auto i:mp)
+        {
+            if(i.second.size()==1)head=i.first;
+        }
+        set<int>vis;
+        vector<int>ans;
+        dfs(mp,vis,head,ans);
+        return ans;
+    }
+};
