ff22f3e5929defd9742b045ed5f622ee9d7ab48f
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-11-23 23:54:25 +0530

    Time: 42 ms (84.42%), Space: 21.5 MB (90.75%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,30 +1,34 @@
-//Dont know about most optimize appraoch
-
 class Solution {
 public:
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
         vector<bool>ans;
+        vector<int>temp;
         for(int i=0;i<l.size();i++)
-        {   vector<int>arr;
+        {
+            temp=nums;
             int s=l[i],e=r[i];
-         
-            for(int i=s;i<=e;i++)
+            sort(temp.begin()+s,temp.begin()+e+1);
+            // for(int j=s;j<=e;j++)cout<<temp[j]<<' ';
+            // cout<<endl;
+            if((e-s+1)==1)
             {
-             arr.push_back(nums[i]);  
+                ans.push_back(false);
             }
-         sort(arr.begin(),arr.end());
-         if(arr.size()==1)ans.push_back(true);
-         else{ bool flag=true;
-             int temp=arr[1]-arr[0];
-             for(int i=1;i<arr.size();i++)
-             {
-                 if(arr[i]-arr[i-1]!=temp){
-                     flag=false;
-                     break;
-                 }
-             }
-              ans.push_back(flag);
-         }
+            else{
+                int dif=temp[s+1]-temp[s];
+                bool flag=true;
+                for(int j=s+1;j<=e;j++)
+                {
+                    if((temp[j]-temp[j-1])!=dif)
+                    {
+                       flag=false;
+                        break;
+                    }
+                }
+                ans.push_back(flag);
+                
+            }
+            
             
         }
         return ans;
