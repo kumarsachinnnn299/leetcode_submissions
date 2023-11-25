f81f6cd77652e8703cf8145158b9af517c8e396e
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-11-25 16:30:21 +0530

    Time: 91 ms (64.32%), Space: 85.9 MB (49.19%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -6,27 +6,27 @@ public:
         int n=nums.size();
         vector<int>sum(n);
         sum[0]=nums[0];     
-        vector<int>ans(n);
+      
         for(int i=1;i<nums.size();i++)sum[i]=nums[i]+sum[i-1];
         for(int i=0;i<nums.size();i++)
         {
             if(i==0)
             {
                 int cr=n-i;
-                ans[i]=sum[n-1]-(nums[i]*cr);
+                nums[i]=sum[n-1]-(nums[i]*cr);
                 
             }
             else if(i==n-1)
             {
                 
-                ans[i]=(nums[i]*n)-sum[i];
+                nums[i]=(nums[i]*n)-sum[i];
             }
             else{
                 int cl=i;
                 int cr=n-i-1;
-                ans[i]=((nums[i]*cl)-sum[i])+(sum[n-1]-(nums[i]*cr)-sum[i-1]);
+                nums[i]=((nums[i]*cl)-sum[i])+(sum[n-1]-(nums[i]*cr)-sum[i-1]);
             }
         }
-        return ans;
+        return nums;
     }
 };
