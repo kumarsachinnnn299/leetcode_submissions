847821fd32689e6308b8b0bc460122fbfe8af70a
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-12-21 14:46:10 +0530

    Time: 272 ms (9.68%), Space: 66.5 MB (87.58%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,21 +1,15 @@
+//Simple bas max x axis difference between two points nikalna h
+
 class Solution {
 public:
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
         sort(points.begin(),points.end());
-        int width=0;
         int ans=0;
         for(int i=1;i<points.size();i++)
         {
             int x=points[i][0]-points[i-1][0];
-            // if(x>width)
-            // {
-            //     width=x;
-            //     int y=points[i][1]-points[i-1][1];
-            //     int area=width*y;
-            //     ans=max(ans,area);
-            // }
-            width=max(x,width);
+            ans=max(x,ans);
         }
-        return width;
+        return ans;
     }
 };
