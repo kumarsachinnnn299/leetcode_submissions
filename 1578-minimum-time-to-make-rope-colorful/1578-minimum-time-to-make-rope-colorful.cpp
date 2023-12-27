757bc268132ba0866872e931ad8e3d59132d5067
SACHIN KUMAR SINGH <77502036+kumarsachinnnn299@users.noreply.github.com>
2023-12-27 12:38:50 +0530

    Time: 115 ms (70.49%), Space: 109.2 MB (8.35%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -3,25 +3,26 @@ public:
     
     //Done
     
-    int helper(int idx,string &s, vector<int>&time)
+    int helper(int idx,string &s, vector<int>&time,vector<int>&dp)
     {
         if(idx>=s.size())return 0;
+        if(dp[idx]!=-1)return dp[idx];
         if(idx>0&&s[idx]==s[idx-1]){
             if(time[idx]<time[idx-1])
             {
                 swap(time[idx-1],time[idx]);
             }
-            return min(time[idx],time[idx-1])+helper(idx+1,s,time);
+            return dp[idx]=min(time[idx],time[idx-1])+helper(idx+1,s,time,dp);
         }
         else{
-            return helper(idx+1,s,time);
+            return dp[idx]=helper(idx+1,s,time,dp);
         }
     }
     
     int minCost(string s, vector<int>& time) {
         
-        
-        return helper(0,s,time);
+        vector<int>dp(s.size(),-1);
+        return helper(0,s,time,dp);
         
     }
 };
