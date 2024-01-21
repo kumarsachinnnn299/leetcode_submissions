48a1abfbb3d197363176fde6b681a67ced20a07b
Sachin Kumar Singh <kumarsachinnnn299@gmail.com>
2024-01-21 15:31:40 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,24 @@
-////Subsets wale question ki type ka h
-
-//rohan ne hint di
-//khud kiya
-
-//Done again during fraz recursion playlist
-
-int helper(int idx,vector<int>&nums,vector<int>&dp)
-{
-    if(idx>=nums.size())return 0;
-    if(dp[idx]!=-1)return dp[idx];
-    int taken=nums[idx]+helper(idx+2,nums,dp);
-    int nt=helper(idx+1,nums,dp);
-    return dp[idx]=max(taken,nt);
-}
-
 class Solution {
 public:
+    
+    int helper(int idx,vector<int>&nums,bool prevtaken,vector<vector<int>>&dp)
+    {
+        if(idx==nums.size())return 0;
+        if(dp[idx][prevtaken]!=-1)return dp[idx][prevtaken];
+        if(prevtaken)
+        {
+            return  dp[idx][prevtaken]=helper(idx+1,nums,!prevtaken,dp);
+        }
+        else{
+            int t=0,nt=0;
+            t=nums[idx]+helper(idx+1,nums,!prevtaken,dp);
+            nt=helper(idx+1,nums,prevtaken,dp);
+            return  dp[idx][prevtaken]= max(t,nt);
+        }
+    }
+    
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return helper(0,nums,dp);
-        
+        vector<vector<int>>dp(nums.size(),vector<int>(2,-1));
+        return helper(0,nums,false,dp);
     }
 };
